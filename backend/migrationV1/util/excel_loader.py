71e73cb354c20d5c0a340cdafccfa5c14b61860a
vmulle <pythontestingforvick@gmail.com>
2024-09-09 14:37:25 -0400

    logging to track progress of file opening

--- backend/migrationV1/util/excel_loader.py
+++ backend/migrationV1/util/excel_loader.py
@@ -16,12 +16,16 @@ class ExcelLoader:
     def load_data(self):
         available_sheet_names = pd.ExcelFile(self.file_path).sheet_names
         logger.info(f"Available sheets: {available_sheet_names}")
-        current_sheet_names = ['Bill', 'BillItemLine', 'SalesReceipt', 'SalesReceiptItemLine', 'Customer']
         
+        logger.info(f'Loading Bill table')
         bills_df = self.load_sheet('Bill') if 'Bill' in available_sheet_names else pd.DataFrame()
+        logger.info(f'Loading BillItem table')
         bill_items_df = self.load_sheet('BillItemLine') if 'BillItemLine' in available_sheet_names else pd.DataFrame()
+        logger.info(f'Loading SalesReceipt table')
         sales_receipts_df = self.load_sheet('SalesReceipt') if 'SalesReceipt' in available_sheet_names else pd.DataFrame()
+        logger.info(f'Loading SalesReceiptItemLine table')
         sales_receipt_items_df = self.load_sheet('SalesReceiptItemLine') if 'SalesReceiptItemLine' in available_sheet_names else pd.DataFrame()
+        logger.info(f'Loading Customer table')
         customers_df = self.load_sheet('Customer') if 'Customer' in available_sheet_names else pd.DataFrame()
         
         return bills_df, bill_items_df, sales_receipts_df, sales_receipt_items_df, customers_df
